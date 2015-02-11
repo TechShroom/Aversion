4349ecd26d21e8d3fdb159cc2262d692e23b7f08
Kenzie Togami <ket1999@gmail.com>
2015-02-10 22:08:31 -0800

    Updates to maven

--- src/main/groovy/maven/plugin.groovy
+++ src/main/groovy/maven/plugin.groovy
@@ -2,14 +2,14 @@ package maven
 import org.gradle.api.*
 import org.gradle.api.tasks.*
 import org.gradle.api.artifacts.ProjectDependency
-public class projectData {
-    boolean gitio = false
-    List<String> otherLocations = []
+class projectData {
+    def boolean gitio = false
+    def List<String> otherLocations = []
 }
 
 public class plugin implements Plugin<Project> {
     void apply(Project project) {
-        project.extensions.create("mavencfg", projectData)
+        project.extensions.create("mavencfg", maven.projectData)
         project.apply plugin: 'maven'
         if (project.mavencfg.gitio) {
             project.uploadArchives {
