0b6a489d7de4a1e9019735af3ee03cf10f80e3a9
Kenzie Togami <ket1999@gmail.com>
2016-04-07 18:30:33 -0700

    this is why i need a syntax checker.

--- upload-archives-travis.sh
+++ upload-archives-travis.sh
@@ -5,7 +5,7 @@ if [[ $TRAVIS_PULL_REQUEST == true ]]; then
     echo 'Aborting due to PR status.'
     exit 0
 fi
-if [[ $TRAVIS_BRANCH == 'dev/version/'* ]] || || [[ $TRAVIS_BRANCH == 'master' ]]; then
+if [[ $TRAVIS_BRANCH == 'dev/version/'* ]] || [[ $TRAVIS_BRANCH == 'master' ]]; then
     echo 'Aborting due to branch being '"$TRAVIS_BRANCH"'.'
     exit 0
 fi
